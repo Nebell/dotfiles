14cb0d21faeb33ca775fc4d1fe941a7914544152
Nebell <a1322049727@foxmail.com>
2024-10-01 00:18:06 +0800

    smartdns domains: reserve more domains

--- smartdns/dnsmasq.china-domain-list.filter.sh
+++ smartdns/dnsmasq.china-domain-list.filter.sh
@@ -10,12 +10,12 @@ OUTPUT_FILE="smartdns.list.out.conf"
 curl -fkLo "${DOMAINS_LIST_FILE}" "${DOMAINS_LIST_URL}"
 
 # filter length in 3 to 8 words and '.com' domain
-rg -Ne 'domain-rules /\S{3,8}\.com' "${DOMAINS_LIST_FILE}" |\
+rg -Ne 'domain-rules /\S{3,11}\.com' "${DOMAINS_LIST_FILE}" |\
     # strip the domains which is:
     # 1. begining with number
     # 2. including '-'
-    # 3. begining 1 to 3 words and the rest is number
+    # 3. begining 1 to 5 words and the rest is number
     # 4. begining a word then a number and the rest are words
-    # rg -Nv 'domain-rules /(\d\S*|[^-]*-\S*|\w{1,3}\d*|\w\d\w*)\.com' |\
-    rg -Nv 'domain-rules /(\d\S*|[^-]*-\S*|\w*\d\S*|zz\w*)\.com' |\
+    rg -Nv 'domain-rules /(\d\S*|[^-]*-\S*|\w{1,5}\d*|\w\d\w*)\.com' |\
+    # rg -Nv 'domain-rules /(\d\S*|[^-]*-\S*|\w*\d\S*|zz\w*)\.com' |\
 	   awk '{print $1,$2,"-nameserver china"}' > "${OUTPUT_FILE}"
