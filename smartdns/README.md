38bab9cd240deadc3d6a890f5420dfdfe2b5acc7
Nebell <a1322049727@foxmail.com>
2024-10-01 01:15:00 +0800

    smartdns

---
+++ smartdns/README.md
@@ -0,0 +1,23 @@
+# 路由器 smartdns 用法
+路由器系统为 *Openwrt*
+
+
+`smartdns.conf` 放置目录为 `/var/etc/smartdns/smartdns.conf`。
+    + `smartdns.conf`里使用参数`-group china`为国内分流
+    + 并使用`-exclude-default-group`从默认组中排除
+
+``` conf
+server 223.5.5.5  -group china -bootstrap-dns -exclude-default-group
+server-tls dot.pub  -group china
+```
+
+
+使用`dnsmasq.china-domain-list.filter.sh`生成简化过后的国内域名列表`china.domains.list`，
+放置在路由器`/etc/smartdns/`目录。
+
+
+在`/etc/smartdns/`中的`custom.conf`或`address.conf`中加入分流参数：
+``` conf
+domain-set -name china -file /etc/smartdns/china.domains.list
+nameserver /domain-set:china/china
+```
--- smartdns/dnsmasq.china-domain-list.filter.sh
+++ smartdns/dnsmasq.china-domain-list.filter.sh
@@ -4,7 +4,7 @@ DOMAINS_LIST_URL="https://fastly.jsdelivr.net/gh/imyip/dnsmasq-china-list-for-sm
 
 DOMAINS_LIST_FILE="accelerated-domains.china.domain.smartdns.conf"
 
-OUTPUT_FILE="smartdns.list.out.conf"
+OUTPUT_FILE="china.domains.list"
 
 # download the list
 curl -fkLo "${DOMAINS_LIST_FILE}" "${DOMAINS_LIST_URL}"
@@ -17,5 +17,5 @@ rg -Ne 'domain-rules /\S{3,11}\.com' "${DOMAINS_LIST_FILE}" |\
     # 3. begining 1 to 5 words and the rest is number
     # 4. begining a word then a number and the rest are words
     rg -Nv 'domain-rules /(\d\S*|[^-]*-\S*|\w{1,5}\d*|\w\d\w*)\.com' |\
-    # rg -Nv 'domain-rules /(\d\S*|[^-]*-\S*|\w*\d\S*|zz\w*)\.com' |\
-	   awk '{print $1,$2,"-nameserver china"}' > "${OUTPUT_FILE}"
+	   # save only domains
+	   awk '{print $2}' | cut -d'/' -f2 > "${OUTPUT_FILE}"
